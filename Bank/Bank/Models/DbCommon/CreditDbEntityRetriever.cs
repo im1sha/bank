e16d59f865e36f5887f67805cb2cbc41997c834b
Mikhail Ovchelupov <mikefoxx2000@ya.ru>
2020-02-26 14:08:21 +0300

    Implement initial version of CreditDbEntityRetriever

--- Bank/Bank/Models/DbCommon/CreditDbEntityRetriever.cs
+++ Bank/Bank/Models/DbCommon/CreditDbEntityRetriever.cs
@@ -1,128 +1,109 @@
-﻿//using Bank.Models;
-//using Microsoft.EntityFrameworkCore;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
 
-//namespace Bank.Models
-//{
-//    public class CreditDbEntityRetriever
-//    {
-//        public BankAppDbContext Db { get; }
+namespace Bank.Models
+{
+    public class CreditDbEntityRetriever
+    {
+        public BankAppDbContext Db { get; }
 
-//        public CreditDbEntityRetriever(BankAppDbContext context)
-//        {
-//            Db = context;
-//        }
+        public CreditDbEntityRetriever(BankAppDbContext context)
+        {
+            Db = context;
+        }
 
-//        public List<Account> GetAccounts() 
-//        {
-//            return Db.Accounts
-//                .Include(i => i.Money).ThenInclude(i => i.Currency)
-//                .Include(i => i.DepositAccount).ThenInclude(i => i.Person).ThenInclude(i => i.Passport)
-//                .Include(i => i.DepositAccount).ThenInclude(i => i.DepositCore).ThenInclude(i => i.DepositVariable).ThenInclude(i => i.DepositGeneral)
-//                .Include(i => i.StandardAccount)
-//                .OrderBy(i => i.Id)
-//                .ToList();
-//        }
+        public List<Account> GetAccounts()
+        {
+            return Db.Accounts
+                .Include(i => i.Money).ThenInclude(i => i.Currency)
+                .Include(i => i.CreditAccount).ThenInclude(i => i.Person).ThenInclude(i => i.Passport)
+                .Include(i => i.CreditAccount).ThenInclude(i => i.CreditTerm)
+                .Include(i => i.StandardAccount)
+                .OrderBy(i => i.Id)
+                .ToList();
+        }
 
-//        public List<Currency> GetCurrencies()
-//        {
-//            return Db.Currencies
-//                .Include(i => i.DepositVariables)
-//                .Include(i => i.Moneys)
-//                .OrderBy(i => i.Id)
-//                .ToList();
-//        }
+        public List<Currency> GetCurrencies()
+        {
+            return Db.Currencies
+                .Include(i => i.Moneys)
+                .OrderBy(i => i.Id)
+                .ToList();
+        }
 
-//        public List<DepositAccount> GetDepositAccounts()
-//        {
-//            return Db.DepositAccounts
-//                .Include(i => i.Account).ThenInclude(i => i.Money)
-//                .Include(i => i.Person).ThenInclude(i => i.Passport)
-//                .Include(i => i.DepositCore).ThenInclude(i => i.InterestAccrual)
-//                .Include(i => i.DepositCore).ThenInclude(i => i.DepositVariable).ThenInclude(i => i.Currency)
-//                .Include(i => i.DepositCore).ThenInclude(i => i.DepositVariable).ThenInclude(i => i.DepositGeneral)
-//                .Include(i => i.Profit)
-//                .OrderBy(i => i.Id)
-//                .ToList();
-//        }
+        public List<CreditAccount> GetCreditAccounts()
+        {
+            return Db.CreditAccounts
+                .Include(i => i.Account).ThenInclude(i => i.Money)
+                .Include(i => i.Person).ThenInclude(i => i.Passport)
+                .Include(i => i.CreditTerm).ThenInclude(i => i.InterestAccrual)
+                .Include(i => i.Fine)
+                .Include(i => i.PaidFinePart)
+                .Include(i => i.PaidMainPart)
+                .Include(i => i.PaidPercentagePart)
+                .OrderBy(i => i.Id)
+                .ToList();
+        }
 
-//        public List<DepositCore> GetDepositCores()
-//        {
-//            return Db.DepositCores
-//                .Include(i => i.DepositVariable).ThenInclude(i => i.DepositGeneral)
-//                .Include(i => i.InterestAccrual)
-//                .Include(i => i.DepositAccounts)
-//                .OrderBy(i => i.InterestAccrualId)
-//                .ThenBy(i => i.DepositVariableId)
-//                .ToList();
-//        }
+        public List<CreditTerm> GetCreditTerms()
+        {
+            return Db.CreditTerms
+                .Include(i => i.InterestAccrual)
+                .Include(i => i.CreditAccounts)
+                .OrderBy(i => i.Id)
+                .ToList();
+        }
 
-//        public List<DepositGeneral> GetDepositGenerals()
-//        {
-//            return Db.DepositGenerals
-//                .Include(i => i.DepositVariables)
-//                .OrderBy(i => i.Id)
-//                .ToList();
-//        }
+        public List<InterestAccrual> GetInterestAccruals()
+        {
+            return Db.InterestAccruals
+                .Include(i => i.CreditTerms)
+                .OrderBy(i => i.Id)
+                .ToList();
+        }
 
-//        public List<DepositVariable> GetDepositVariables()
-//        {
-//            return Db.DepositVariables
-//                .Include(i => i.Currency)
-//                .Include(i => i.DepositCores)
-//                .Include(i => i.DepositGeneral)
-//                .Include(i => i.MinimalDeposit)
-//                .OrderBy(i => i.Id)
-//                .ToList();
-//        }
+        public List<LegalEntity> GetLegalEntities()
+        {
+            return Db.LegalEntities
+                .Include(i => i.StandardAccounts)
+                .OrderBy(i => i.Id)
+                .ToList();
+        }
 
-//        public List<InterestAccrual> GetInterestAccruals()
-//        {
-//            return Db.InterestAccruals
-//                .Include(i => i.DepositCores)
-//                .OrderBy(i => i.Id)
-//                .ToList();
-//        }
+        public List<Money> GetMoneys()
+        {
+            return Db.Moneys
+                .Include(i => i.Currency)
+                .Include(i => i.Account)
+                .Include(i => i.CreditAccountFine)
+                .Include(i => i.CreditAccountPaidFinePart)
+                .Include(i => i.CreditAccountPaidMainPart)
+                .Include(i => i.CreditAccountPaidPercentagePart)
+                .Include(i => i.CreditTermMaximalCredit)
+                .Include(i => i.CreditTermMinimalCredit)
+                .Include(i => i.Transaction)
+                .OrderBy(i => i.Id)
+                .ToList();
+        }
 
-//        public List<LegalEntity> GetLegalEntities()
-//        {
-//            return Db.LegalEntities
-//                .Include(i => i.StandardAccounts)
-//                .OrderBy(i => i.Id)
-//                .ToList(); //
-//        }
+        public List<StandardAccount> GetStandardAccounts()
+        {
+            return Db.StandardAccounts
+                .Include(i => i.Account).ThenInclude(i => i.Money).ThenInclude(i => i.Currency)
+                .Include(i => i.Person).ThenInclude(i => i.Passport)
+                .Include(i => i.LegalEntity)
+                .OrderBy(i => i.Id)
+                .ToList();
+        }
 
-//        public List<Money> GetMoneys()
-//        {
-//            return Db.Moneys
-//                .Include(i => i.Currency)
-//                .Include(i => i.Account)
-//                .Include(i => i.DepositVariable)
-//                .Include(i => i.Transaction)
-//                .Include(i => i.DepositAccount)
-//                .OrderBy(i => i.Id)
-//                .ToList();
-//        }
-
-//        public List<StandardAccount> GetStandardAccounts()
-//        {
-//            return Db.StandardAccounts
-//                .Include(i => i.Account).ThenInclude(i => i.Money).ThenInclude(i => i.Currency)
-//                .Include(i => i.Person).ThenInclude(i => i.Passport)
-//                .Include(i => i.LegalEntity)
-//                .OrderBy(i => i.Id)
-//                .ToList();
-//        }
-
-//        public List<Transaction> GetTransactions()
-//        {
-//            return Db.Transactions
-//                .Include(i => i.Account)
-//                .Include(i => i.Amount)
-//                .OrderBy(i => i.Id)
-//                .ToList();
-//        }
-//    }
-//}
+        public List<Transaction> GetTransactions()
+        {
+            return Db.Transactions
+                .Include(i => i.Account)
+                .Include(i => i.Amount)
+                .OrderBy(i => i.Id)
+                .ToList();
+        }
+    }
+}
